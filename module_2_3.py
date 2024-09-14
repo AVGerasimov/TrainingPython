1181ce2290c9a82c99d3c7d8874e69f434c8c61f
Андрей Герасимов <teletv@mail.ru>
2024-09-14 16:03:47 +0700

    Yome work

---
+++ module_2_3.py
@@ -0,0 +1,14 @@
+
+
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+#rint(len (my_list))
+i=0
+while i < len (my_list):
+  if my_list[i]>0:
+    print(my_list[i])
+  else:
+    if my_list[i]<0:
+      break
+  i+=1
+
