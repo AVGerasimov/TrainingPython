151cfd004fab8dd3594f04a30865249f430f1dc0
Андрей Герасимов <teletv@mail.ru>
2024-09-28 05:39:39 +0700

    Correct

--- module_3_5.py
+++ module_3_5.py
@@ -1,13 +1,16 @@
 __author__ = 'Андрей Герасимов'
+# Программа показывает алгоритм рекурсии. Требуется рассчитать произведение всех цифр числа
 
 def get_multiplied_digits(number):
+    while number%10 == 0:   # данный цикл выполняется до тех пор, пока все возможные нули в конце числа не уберутся
+        number = number//10
     str_number = str(number)
-    first = int (str_number[0])
+    first = int (str_number[0]) # приводя к целому, убираем возможные нули в начале числа
 
     if len (str_number) > 1:
         return (first * get_multiplied_digits(int(str_number[1:])))
     else:
         return (first)
 
-Result = get_multiplied_digits(40203)
+Result = get_multiplied_digits(4020300)
 print(Result)
