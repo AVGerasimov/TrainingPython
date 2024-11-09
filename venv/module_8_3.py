864866bb33d92a5bda6c4a577c1b167b28242e9d
Андрей Герасимов <teletv@mail.ru>
2024-11-09 19:09:31 +0700

    send1e

--- venv/module_8_3.py
+++ venv/module_8_3.py
@@ -1,5 +1,9 @@
 __author__ = 'Андрей Герасимов'
 
+
+class IncorrectVinNumber(Exception):
+    pass
+
 class Car:
     def __init__(self, model ,__vin , __numbers ):
         self.model = model
@@ -10,11 +14,11 @@ class Car:
 
         def __is_valid_vin(vin_number):
             if isinstance(vin_number, int)== False:
-                raise NameError ('Некорректный тип vin номер')
+                raise IncorrectVinNumber ('Некорректный тип vin номер')
             if vin_number not in range(1000000,9999999):
-                raise NameError('Неверный диапазон для vin номера')
+                raise IncorrectVinNumber('Неверный диапазон для vin номера')
 
-        __is_valid_vin(self.__numbers)
+        __is_valid_vin(self.__vin)
 
         def __is_valid_numbers(numbers):
             pass
@@ -23,5 +27,5 @@ class Car:
 try:
     first = Car('Model1', 1000001, 'f123dj')
 
-except NameError as exc1:
+except IncorrectVinNumber as exc1:
     print(f'wow {exc1}   {type(exc1)}')
