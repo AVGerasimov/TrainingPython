1a331b80824dffb56da30d86892815a042c1cbe0
Андрей Герасимов <teletv@mail.ru>
2024-12-04 23:53:01 +0700

    mult

---
+++ venv/module_10_5.py
@@ -0,0 +1,50 @@
+import time
+import multiprocessing
+from multiprocessing import Process
+from multiprocessing import Pool
+
+
+def  read_info(name):
+    all_data = []
+    with open (name,'r', encoding='utf-8') as file:
+        line = file.readline()
+        all_data.append(line)
+        while line != '':
+            line = file.readline()
+            if line != '':
+                all_data.append(line)
+    return all_data
+
+
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+t1 = time.time()
+print(t1)
+for i in range(1, 5):
+    read_info(filenames[i-1])
+    print(f" прочитан файл номер {i-1}")
+t2 = time.time()
+print(t2)
+delta = t2-t1
+print(f' время линейный способ чтения{delta}')
+
+
+t1 = time.time()
+print(t1)
+if __name__ == '__main__':
+    # Создание пула из 4 процессов
+    with Pool(processes=4) as pool:
+        # Входной список значений
+        values = filenames
+        # Применение функции read_info к каждому элементу в списке параллельно
+        results = pool.map(read_info, values)
+
+t2 = time.time()
+print(t2)
+delta = t2-t1
+print(f' время многопроцессорный способ чтения{delta}')
+
+
+
+
+
