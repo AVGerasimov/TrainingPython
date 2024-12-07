8cce8d8460fc1d7ce6e1b0f59f086b9800913bdb
Андрей Герасимов <teletv@mail.ru>
2024-12-07 20:56:26 +0700

    tr

--- venv/module_10_5.py
+++ venv/module_10_5.py
@@ -1,56 +1,48 @@
 import time
-import multiprocessing
-from multiprocessing import Process
 from multiprocessing import Pool
 
-timemulti = 0
 
-def  read_info(name):
-    global timemulti
-    t1 = time.time()
+def read_info(name):
+    start_time = time.time()
     all_data = []
-    with open (name,'r', encoding='utf-8') as file:
+    with open(name, 'r', encoding='utf-8') as file:
         line = file.readline()
         all_data.append(line)
         while line != '':
             line = file.readline()
             if line != '':
                 all_data.append(line)
-    print(f" прочитан файл номер {file.name}")
-    t2 = time.time()
-    delta = t2 - t1
-    print(f" Время чтения файла {file.name} = {delta}")
-    timemulti+=delta
-    print(f"timemulti={timemulti}")
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Файл {name} прочитан за {elapsed_time:.3f} с.")
     return all_data
 
 
 filenames = [f'./file {number}.txt' for number in range(1, 5)]
 
-# t1 = time.time()
-# print(f' Начало линейного способа, время = {t1}')
-# for i in range(1, 5):
-#     read_info(filenames[i-1])
-#     print(f" прочитан файл номер {i-1}")
-# t2 = time.time()
-# print(t2)
-# delta = t2-t1
-# print(f' время линейный способ чтения, результат = {delta}')
 
 
 t1 = time.time()
-print(f' Начало многопроцессорного способа, время = {t1}')
+print(f' Начало линейного способа, время = {t1}')
+for i in range(1, 5):
+    read_info(filenames[i-1])
+    print(f" прочитан файл номер {i-1}")
+t2 = time.time()
+print(t2)
+delta = t2-t1
+print(f' время линейный способ чтения, результат = {delta}')
+
+
+
+
 if __name__ == '__main__':
-    # Создание пула из 4 процессов
+    t1 = time.time()
+    print(f'Начало многопроцессорного способа, время = {t1:.3f} с.')
+
     with Pool(processes=4) as pool:
-        # Входной список значений
-        # Применение функции read_info к каждому элементу в списке параллельно
         results = pool.map(read_info, filenames)
 
-
-print(f' Время многопроцессорный способ чтения = {timemulti}')
-
-
-
-
-
+    t2 = time.time()
+    delta = t2 - t1
+    print(f'Конец многопроцессорного способа, время = {t2:.3f} с.')
+    print(f'Общее время многопроцессорного способа чтения: {delta:.3f} с.')
