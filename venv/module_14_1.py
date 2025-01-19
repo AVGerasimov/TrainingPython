5dee2259c69992167308d9b50c44c945d0ad9c26
Андрей Герасимов <teletv@mail.ru>
2025-01-20 00:05:36 +0700

    next

--- venv/module_14_1.py
+++ venv/module_14_1.py
@@ -35,8 +35,34 @@ for i in range(1, 11):
 
 cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
 users = cursor.fetchall()
+s = 0
 for user in users:
+    s+=1
     print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+    print(f'counter= {s}')
+
+
+#cursor.execute("SELECT MIN(age) FROM Users")
+#total3 = cursor.fetchone()[0]
+#print(f"function MIN age = {total3}")
+
+cursor.execute('''DELETE FROM Users WHERE id == 6''') # удаление всех данных таблицы Users c id == 6
+
+cursor.execute("SELECT COUNT(*) FROM Users")
+total1 = cursor.fetchone()[0]
+print(f"Общее количество пользователей = {total1}")
+
+cursor.execute("SELECT SUM(balance) FROM Users")
+total2 = cursor.fetchone()[0]
+print(f"sum balance = {total2}")
+
+cursor.execute("SELECT AVG(balance) FROM Users")
+total3 = cursor.fetchone()[0]
+print(f"Average balance = {total3}")
+
+
+#cursor.execute('''DELETE FROM Users''') # удаление всех данных таблицы Users
 
 connection.commit()
 connection.close()
+
